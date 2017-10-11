b20496ba8a2acc41b43ea327a8beb8ea4f821e30
Caleb Evans <caleb@calebevans.me>
2017-10-11 14:49:34 -0700

    Add time calculations UI

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-(function (m, moment) {
+(function (m, moment, leftPad) {
 
 let logTimeFormat = 'h:mma';
 
 class AppComponent {
+
+  constructor() {
+    this.logText = localStorage.getItem('logText');
+    if (this.logText) {
+      this.log = this.parseTextLog(this.logText);
+    }
+  }
 
   getLineDepth(logLine) {
     return Math.round(logLine.search(/\S/) / 4);
@@ -34,25 +41,26 @@ class AppComponent {
     }
   }
 
-  getClients(logText) {
+  getCategories(logText) {
 
     let logLines = logText.split('\n');
-    let clients = [];
-    let currentClient = null;
+    let categories = [];
+    let currentCategory = null;
 
     logLines.forEach((logLine) => {
       if (logLine.trim() !== '') {
         let lineDepth = this.getLineDepth(logLine);
         if (lineDepth === 0) {
-          // This is a top-level client (or Internal)
-          currentClient = {
+          // This is a top-level category (e.g. Tyme, Internal, etc.)
+          currentCategory = {
+            name: this.getLineContent(logLine),
             timeRanges: []
           };
-          clients.push(currentClient);
+          categories.push(currentCategory);
         } else if (lineDepth === 1) {
           // This is a time range string
           let timeStrs = this.parseLineTimes(logLine);
-          currentClient.timeRanges.push({
+          currentCategory.timeRanges.push({
             startTime: moment(timeStrs[0], logTimeFormat),
             endTime: moment(timeStrs[1], logTimeFormat)
           });
@@ -60,24 +68,38 @@ class AppComponent {
       }
     });
 
-    return clients;
+    return categories;
 
   }
 
-  calculateClientSums(clients) {
-    clients.forEach((client) => {
-      client.timeRanges.forEach((timeRange) => {
-        console.log(timeRange);
+  calculateTotals(log) {
+    log.totalDuration = moment.duration(0);
+    log.categories.forEach((category) => {
+      category.totalDuration = moment.duration(0);
+      category.timeRanges.forEach((timeRange) => {
+        timeRange.totalDuration = moment.duration(
+          timeRange.endTime.diff(timeRange.startTime));
+        category.totalDuration.add(timeRange.totalDuration);
       });
+      log.totalDuration.add(category.totalDuration);
     });
   }
 
+  getFormattedDuration(duration) {
+    let hours = duration.hours();
+    let minutes = leftPad(duration.minutes(), 2, '0');
+    return `${hours}:${minutes}`;
+  }
+
   parseTextLog(logText) {
+    this.log = {};
+    this.log.categories = this.getCategories(logText);
+    this.calculateTotals(this.log);
+    return this.log;
+  }
 
-    let clients = this.getClients(logText);
-    this.calculateClientSums(clients);
-    console.log(clients);
-
+  saveTextLog(logText) {
+    localStorage.setItem('logText', logText);
   }
 
   view() {
@@ -86,17 +108,35 @@ class AppComponent {
         m('h1', 'Workday Time Calculator')
       ]),
       m('div.app-content', [
+
         m('textarea.log-input', {
           // TODO: uncomment this
           // autofocus: true,
           placeholder: 'Paste your time log here',
           oninput: (event) => {
+            this.logText = event.target.value;
             this.parseTextLog(event.target.value);
-            localStorage.setItem('logText', event.target.value);
+            this.saveTextLog(event.target.value);
           },
-          oncreate: (vnode) => this.parseTextLog(vnode.dom.value)
-        }, localStorage.getItem('logText')),
-        m('div.log-calculations')
+        }, this.logText),
+
+        this.log ?
+        m('div.log-calculations', [
+          this.log.categories.map((category) => {
+            return m('div.log-category', [
+              m('div.log-category-name.log-label', `${category.name}:`),
+              ' ',
+              m('div.log-category-total-time.log-value', [
+                this.getFormattedDuration(category.totalDuration)
+              ])
+            ]);
+          }),
+          m('div.log-total', [
+            m('div.log-total-time-name.log-label', 'Total'),
+            m('div.log-total-time.log-value', this.getFormattedDuration(this.log.totalDuration))
+          ])
+        ]) : null
+
       ])
     ]);
   }
@@ -105,4 +145,4 @@ class AppComponent {
 
 m.mount(document.body, AppComponent);
 
-}(window.m, window.moment));
+}(window.m, window.moment, window.leftPad));
