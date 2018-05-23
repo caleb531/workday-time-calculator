9e8a9795f46e05117a4305705c95cac114b52048
Caleb Evans <caleb@calebevans.me>
2018-05-22 18:39:46 -0700

    Allow the use of console.log

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,8 +43,6 @@ module.exports = {
 
     // disallow assignment operators in conditional expressions
     'no-cond-assign': ['error', 'always'],
-    // disallow the use of console
-    'no-console': ['error', {'allow': ['error']}],
     // disallow the use of debugger
     'no-debugger': ['error'],
     // disallow duplicate arguments in function definitions
