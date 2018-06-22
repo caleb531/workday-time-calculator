f45a224f269f1894170c1e8c25755ba348697bab
Caleb Evans <caleb@calebevans.me>
2018-06-21 21:09:00 -0700

    Finish Export functionality

--- app/scripts/export.js
+++ app/scripts/export.js
@@ -1,14 +1,31 @@
 class ExportComponent {
 
-  exportAll() {
-    console.log('export all!');
+  getExportedJson() {
+    let exportedData = {logs: {}};
+    Object.keys(localStorage).forEach((key) => {
+      let logMatches = key.match(/^wtc-date-(\d{1,2}\/\d{1,2}\/\d{4})$/);
+      if (logMatches) {
+        let logDate = logMatches[1];
+        exportedData.logs[logDate] = localStorage.getItem(key);
+      }
+    });
+    return exportedData;
+  }
+
+  downloadExportedJson() {
+    let a = document.createElement('a');
+    a.href = URL.createObjectURL(new Blob([
+      JSON.stringify(this.getExportedJson())
+    ], {type: 'application/json'}));
+    a.download = 'logs.json';
+    a.click();
   }
 
   view() {
     return m('span.app-control-export', {
       onclick: (event) => {
         event.preventDefault();
-        this.exportAll();
+        this.downloadExportedJson();
       }
     }, 'Export All');
   }
