fecf275441b0a7b0e53255ad92f2b0119e2ba793
Caleb Evans <caleb@calebevans.me>
2018-06-23 14:11:46 -0700

    Remove check for existence of workbox
    This will ensure that an error is thrown when workbox does not exist (so
    I'm not left in the dark).

--- app/assets/service-worker.js
+++ app/assets/service-worker.js
@@ -1,10 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
-if (workbox) {
-
-  workbox.routing.registerRoute(
-    /\/|\.(?:html|js|css)$/,
-    workbox.strategies.staleWhileRevalidate()
-  );
-
-}
+workbox.routing.registerRoute(
+  /\/|\.(?:html|js|css)$/,
+  workbox.strategies.staleWhileRevalidate()
+);
