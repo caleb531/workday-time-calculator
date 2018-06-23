c00639fdb9052d27c9ec59e913dab04842298121
Caleb Evans <caleb@calebevans.me>
2018-06-23 11:32:16 -0700

    Properly mount AppComponent to <main>
    This prevents the document <script> tags from being erased when the
    AppComponent is mounted.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,6 @@
 import AppComponent from './app.js';
 
-m.mount(document.body, AppComponent);
+m.mount(document.body.querySelector('main'), AppComponent);
 
 if (navigator.serviceWorker && window.location.protocol === 'https:') {
   navigator.serviceWorker.register('service-worker.js');
