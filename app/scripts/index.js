c990ee4d76d6ebfe53d7208c57d8652d3e3509b2
Caleb Evans <caleb@calebevans.me>
2018-06-22 19:25:50 -0700

    Do not attepmt to load service worker over file URL

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,6 @@ import AppComponent from './app.js';
 
 m.mount(document.body, AppComponent);
 
-if (navigator.serviceWorker) {
+if (navigator.serviceWorker && window.location.protocol === 'https:') {
   navigator.serviceWorker.register('service-worker.js');
 }
