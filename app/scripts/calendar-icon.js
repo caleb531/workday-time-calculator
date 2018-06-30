abfe48cefa2e2c8a7a22c9b4bdfec0f30a5748a7
Caleb Evans <caleb@calebevans.me>
2018-06-30 12:20:41 -0700

    Fix pixel-sized protrusions on sides of calendar icon

--- app/scripts/calendar-icon.js
+++ app/scripts/calendar-icon.js
@@ -3,7 +3,7 @@ class CalendarIconComponent {
   view({attrs}) {
     return m('svg[viewBox="0 0 32 32"]', [
       m('path', {
-        d: 'M 8,8 L 8,24 L 24,24 L 24,8 Z M 8,10 L 24,10 M 10,8 L 10,6 M 22,8 L 22,6'
+        d: 'M 8,8 L 8,24 L 24,24 L 24,8 Z M 9,10 L 23,10 M 10,8 L 10,6 M 22,8 L 22,6'
       }),
       m('text', {
         x: 16, y: 20,
