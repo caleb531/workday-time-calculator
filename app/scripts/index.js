b2adafb8c8a3b240fe93905b90f426440974b175
Caleb Evans <caleb@calebevans.me>
2018-07-04 16:00:32 -0700

    Allow service worker to run under localhost

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,6 @@ import AppComponent from './app.js';
 
 m.mount(document.body.querySelector('main'), AppComponent);
 
-if (navigator.serviceWorker && window.location.protocol === 'https:') {
+if (navigator.serviceWorker) {
   navigator.serviceWorker.register('service-worker.js');
 }
