43ae163a8a8c4330567cb53242e78a6774bbea26
Caleb Evans <caleb@calebevans.me>
2018-09-19 19:46:08 -0700

    Replace float with flex for Latest Time container

--- app/scripts/summary.js
+++ app/scripts/summary.js
@@ -28,6 +28,61 @@ class SummaryComponent {
 
       m('div.log-summary-overview', [
 
+        m('div.log-summary-overview-main', [
+
+          m('div.log-total', [
+            m('div.log-total-time-name.log-label', 'Total:'),
+            ' ',
+            m('div.log-total-time.log-value', this.getFormattedDuration(log.totalDuration))
+          ]),
+
+          m('.log-stats', [
+
+            log.errors.length > 0 ?
+            m('div.log-errors', [
+              m('span.log-label', 'Errors:'),
+              ' ',
+              m('div.log-times.log-error-times', log.errors.map((error) => {
+                return m('div.log-error', [
+                  m('span.log-error-start-time.log-value', error.startTime.isValid() ? error.startTime.format(timeFormatShort) : '?'),
+                  ' to ',
+                  m('span.log-error-end-time.log-value', error.endTime.isValid() ? error.endTime.format(timeFormatShort) : '?')
+                ]);
+              }))
+            ]) : null,
+
+            log.gaps.length > 0 ?
+            m('div.log-gaps', [
+              m('span.log-label', 'Gaps:'),
+              ' ',
+              m('div.log-times.log-gap-times', log.gaps.map((gap) => {
+                return m('div.log-gap', [
+                  m('span.log-gap-start-time.log-value', gap.startTime.isValid() ? gap.startTime.format(timeFormatShort) : '?'),
+                  ' to ',
+                  m('span.log-gap-end-time.log-value', gap.endTime.isValid() ? gap.endTime.format(timeFormatShort) : '?')
+                ]);
+              }))
+            ]) : null,
+
+            log.overlaps.length > 0 ?
+            m('div.log-overlaps', [
+              m('span.log-label', 'Overlaps:'),
+              ' ',
+              m('div.log-times.log-overlap-times', log.overlaps.map((overlap) => {
+                return m('div.log-overlap', [
+                  m('span.log-overlap-start-time.log-value', overlap.startTime.isValid() ? overlap.startTime.format(timeFormatShort) : '?'),
+                  ' to ',
+                  m('span.log-overlap-end-time.log-value', overlap.endTime.isValid() ? overlap.endTime.format(timeFormatShort) : '?'),
+                  ' ',
+                  m('span.log-value-category', `(${overlap.category.name})`)
+                ]);
+              }))
+            ]) : null
+
+          ])
+
+        ]),
+
         log.latestRange ?
         m('div.log-latest-time', [
           m('span.log-label', 'Latest Time:'),
@@ -37,58 +92,7 @@ class SummaryComponent {
             ' ',
             m('span.log-value-category', `(${log.latestRange.category.name})`)
           ])
-        ]) : null,
-
-        m('div.log-total', [
-          m('div.log-total-time-name.log-label', 'Total:'),
-          ' ',
-          m('div.log-total-time.log-value', this.getFormattedDuration(log.totalDuration))
-        ]),
-
-        m('.log-stats', [
-
-          log.errors.length > 0 ?
-          m('div.log-errors', [
-            m('span.log-label', 'Errors:'),
-            ' ',
-            m('div.log-times.log-error-times', log.errors.map((error) => {
-              return m('div.log-error', [
-                m('span.log-error-start-time.log-value', error.startTime.isValid() ? error.startTime.format(timeFormatShort) : '?'),
-                ' to ',
-                m('span.log-error-end-time.log-value', error.endTime.isValid() ? error.endTime.format(timeFormatShort) : '?')
-              ]);
-            }))
-          ]) : null,
-
-          log.gaps.length > 0 ?
-          m('div.log-gaps', [
-            m('span.log-label', 'Gaps:'),
-            ' ',
-            m('div.log-times.log-gap-times', log.gaps.map((gap) => {
-              return m('div.log-gap', [
-                m('span.log-gap-start-time.log-value', gap.startTime.isValid() ? gap.startTime.format(timeFormatShort) : '?'),
-                ' to ',
-                m('span.log-gap-end-time.log-value', gap.endTime.isValid() ? gap.endTime.format(timeFormatShort) : '?')
-              ]);
-            }))
-          ]) : null,
-
-          log.overlaps.length > 0 ?
-          m('div.log-overlaps', [
-            m('span.log-label', 'Overlaps:'),
-            ' ',
-            m('div.log-times.log-overlap-times', log.overlaps.map((overlap) => {
-              return m('div.log-overlap', [
-                m('span.log-overlap-start-time.log-value', overlap.startTime.isValid() ? overlap.startTime.format(timeFormatShort) : '?'),
-                ' to ',
-                m('span.log-overlap-end-time.log-value', overlap.endTime.isValid() ? overlap.endTime.format(timeFormatShort) : '?'),
-                ' ',
-                m('span.log-value-category', `(${overlap.category.name})`)
-              ]);
-            }))
-          ]) : null
-
-        ])
+        ]) : null
 
       ]),
 
