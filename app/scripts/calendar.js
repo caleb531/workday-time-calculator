caleb531/workday-time-calculator

commit e6634a74b00754982ce60042465b27c4a0edfb1a

Embolden today's date in Calendar panel

app/scripts/calendar.js
@@ -12,9 +12,22 @@ class CalendarComponent {
 
   onbeforeupdate({attrs: {selectedDate}}) {
     if (!this.selectedDate || !selectedDate.isSame(this.selectedDate)) {
-      this.selectedDate = selectedDate.clone();
+      this.selectedDate = this.resetTime(selectedDate.clone());
       this.firstDayOfMonthInView = this.getFirstDayOfMonth(this.selectedDate);
     }
+  }
+
+  resetTime(date) {
+    return date.set({
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: 0
+    });
+  }
+
+  getToday() {
+    return this.resetTime(moment());
   }
 
   getFirstDayOfMonth(date) {
@@ -67,10 +80,10 @@ class CalendarComponent {
     if (lastDateInView.diff(firstDateInView, 'days') < (CalendarComponent.daysInWeek * 5)) {
       lastDateInView.add(1, 'week');
     }
-    let currentDate = firstDateInView.clone();
+    let currentDate = this.resetTime(firstDateInView.clone());
     let values = [];
     while (currentDate.isSameOrBefore(lastDateInView)) {
-      values.push(callback(currentDate));
+      values.push(callback(currentDate.clone()));
       currentDate.add(1, 'day');
     }
     return values;
@@ -110,7 +123,7 @@ class CalendarComponent {
             class: [
               currentDate.format('YYYY/MM') === this.firstDayOfMonthInView.format('YYYY/MM') ? 'is-current-month' : '',
               currentDate.isSame(this.selectedDate) ? 'is-selected' : '',
-              currentDate.isSame(moment()) ? 'is-today' : ''
+              currentDate.isSame(this.getToday()) ? 'is-today' : ''
             ].join(' ')
           }, m('div.log-calendar-date-label', currentDate.date()));
         }))

app/styles/_calendar.scss
@@ -109,4 +109,7 @@
   &.is-current-month.is-selected .log-calendar-date-label {
     background-color: $blue;
   }
+  &.is-today .log-calendar-date-label {
+    font-weight: 700;
+  }
 }

commit 881548b3b26de9ca25ba20df8a9cb848d9894422

Fix bug where relative date was miscalculated
This bug was a regression introduced in e6634a7.

app/scripts/calendar.js
@@ -11,9 +11,9 @@ class CalendarComponent {
   }
 
   onbeforeupdate({attrs: {selectedDate}}) {
-    selectedDate = this.resetTime(selectedDate);
+    selectedDate = this.resetTime(selectedDate.clone());
     if (!this.selectedDate || !selectedDate.isSame(this.selectedDate)) {
-      this.selectedDate = this.resetTime(selectedDate.clone());
+      this.selectedDate = this.resetTime(selectedDate);
       this.firstDayOfMonthInView = this.getFirstDayOfMonth(this.selectedDate);
     }
   }