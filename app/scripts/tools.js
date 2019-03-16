3056248092be72d9cff520f5d10b14d81cc53657
Caleb Evans <caleb@calebevans.me>
2019-03-15 21:48:03 -0700

    Add overlay to Tools menu
    Adjust the z-index of calendar elements so calendar control is not fully
    opaque when the tools menu is open.

--- app/scripts/tools.js
+++ app/scripts/tools.js
@@ -9,13 +9,19 @@ class ControlsComponent {
   }
 
   view() {
-    return m('div.app-tools', [
+    return m('div.app-tools', {
+      class: this.menuIsOpen ? 'open' : '',
+    }, [
       m('button.app-tools-menu-toggle', {
-        class: this.menuIsOpen ? 'active' : '',
         onclick: () => {
           this.menuIsOpen = !this.menuIsOpen;
         }
       }, m(WrenchIconComponent)),
+      m('div.dismissable-overlay', {
+        onclick: () => {
+          this.menuIsOpen = false;
+        }
+      }),
       m('ul.app-tools-menu', {
         class: this.menuIsOpen ? 'open' : '',
         // Close menu when menu item is clicked
