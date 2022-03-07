70d526e1d02ce696e95152bdc478b8b03efe0f62
Caleb Evans <caleb@calebevans.me>
2022-03-06 20:27:22 -0800

    Ensure autocomplete stays enabled if future modes are defined

--- app/scripts/components/editor.js
+++ app/scripts/components/editor.js
@@ -12,11 +12,11 @@ class EditorComponent {
     this.selectedDate = selectedDate.clone();
     this.onSetLogContents = onSetLogContents;
     this.autocompleter = new EditorAutocompleter({
-      isEnabled: this.preferences.autocompleteMode === 'on',
+      isEnabled: this.preferences.autocompleteMode !== 'off',
       onReceiveCompletions: () => m.redraw()
     });
     this.preferences.on('change:autocompleteMode', (key, newMode) => {
-      this.autocompleter.setIsEnabled(newMode === 'on');
+      this.autocompleter.setIsEnabled(newMode !== 'off');
     });
   }
 
