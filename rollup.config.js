2007e8ce68abd8744512efdc44b617a939a8a545
Caleb Evans <caleb@calebevans.me>
2022-08-08 10:34:17 -0700

    Fix broken Offline mode
    The glob directory used for SW precaching should be dist/, not public/.

--- rollup.config.js
+++ rollup.config.js
@@ -64,7 +64,7 @@ export default {
     json(),
     process.env.NODE_ENV === 'production' ? terser() : null,
     injectManifest({
-      globDirectory: 'public',
+      globDirectory: 'dist',
       globPatterns: [
         '**\/*.{js,css,png}',
         'icons/*.svg'
