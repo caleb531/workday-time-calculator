9b3f17fab29fc383cb2030f8d4f0589b35ae15ce
Caleb Evans <caleb@calebevans.me>
2023-05-21 16:27:37 -0700

    Upgrade to @fontsource v5
    Also switch to the default font-display:swap, since it does not seem to
    have any adverse effect.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import m from 'mithril';
 import AppComponent from './components/app.js';
 import '../styles/index.scss';
+import '@fontsource-variable/open-sans/wght.css';
 
 m.mount(document.body.querySelector('main'), AppComponent);
