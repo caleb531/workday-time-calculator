418a3d41d07bccfc011205b16dfacc7a428adaf9
Caleb Evans <caleb@calebevans.me>
2023-07-22 14:41:38 -0700

    Add test for minutewise times

--- tests/log.test.js
+++ tests/log.test.js
@@ -2,6 +2,7 @@ import moment from 'moment';
 import Log from '../scripts/models/log';
 import basicLogContents from './test-logs/basic.json';
 import noDescriptionsLogContents from './test-logs/no-descriptions.json';
+import minutewiseLogContents from './test-logs/minutewise.json';
 import overlapLeftLogContents from './test-logs/overlap-left.json';
 import overlapRightLogContents from './test-logs/overlap-right.json';
 import overlapInnerLogContents from './test-logs/overlap-inner.json';
@@ -45,6 +46,23 @@ describe('Log model', () => {
     expect(category.tasks[1].totalDuration).toEqualDuration(210, 'minutes');
     expect(category.totalDuration).toEqualDuration(225, 'minutes');
     expect(log.totalDuration).toEqualDuration(225, 'minutes');
+    expect(log.errors).toHaveLength(0);
+    expect(log.gaps).toHaveLength(0);
+    expect(log.overlaps).toHaveLength(0);
+  });
+
+  it('should allow for minute-wise times', () => {
+    const log = createLog(minutewiseLogContents);
+    const category = log.categories[0];
+    expect(category).toHaveProperty('name', 'Internal');
+    expect(category.tasks[0].startTime).toEqualTime('8:48am');
+    expect(category.tasks[0].endTime).toEqualTime('8:55am');
+    expect(category.tasks[1].startTime).toEqualTime('8:55am');
+    expect(category.tasks[1].endTime).toEqualTime('9:06am');
+    expect(category.tasks[0].totalDuration).toEqualDuration(7, 'minutes');
+    expect(category.tasks[1].totalDuration).toEqualDuration(11, 'minutes');
+    expect(category.totalDuration).toEqualDuration(18, 'minutes');
+    expect(log.totalDuration).toEqualDuration(18, 'minutes');
     expect(log.errors).toHaveLength(0);
     expect(log.gaps).toHaveLength(0);
     expect(log.overlaps).toHaveLength(0);
