4b66420291ad1e8d7f9bb61037da633d78a42c35
Caleb Evans <caleb@calebevans.me>
2023-07-23 16:54:07 -0700

    Do not require every property on log to be asserted

--- tests/log.test.js
+++ tests/log.test.js
@@ -18,38 +18,50 @@ describe('Log model', () => {
     it(testCase.description, () => {
       const log = createLog(testCase.logContents);
 
-      testCase.assertions.categories.forEach((expectedCategory, c) => {
-        const category = log.categories[c];
-        expect(category).toHaveProperty('name', expectedCategory.name);
-        expect(category).toHaveProperty('descriptions', expectedCategory.descriptions);
-        expectedCategory.tasks.forEach((expectedTask, t) => {
-          const task = category.tasks[t];
-          expect(task.startTime).toEqualTime(expectedTask.startTime);
-          expect(task.endTime).toEqualTime(expectedTask.endTime);
-          expect(task.totalDuration).toEqualDuration(expectedTask.totalDuration, 'minutes');
+      if (testCase.assertions.categories) {
+        testCase.assertions.categories.forEach((expectedCategory, c) => {
+          const category = log.categories[c];
+          expect(category).toHaveProperty('name', expectedCategory.name);
+          expect(category).toHaveProperty('descriptions', expectedCategory.descriptions);
+          if (expectedCategory.tasks) {
+            expectedCategory.tasks.forEach((expectedTask, t) => {
+              const task = category.tasks[t];
+              expect(task.startTime).toEqualTime(expectedTask.startTime);
+              expect(task.endTime).toEqualTime(expectedTask.endTime);
+              expect(task.totalDuration).toEqualDuration(expectedTask.totalDuration, 'minutes');
+            });
+            expect(category.tasks).toHaveLength(expectedCategory.tasks.length);
+          }
+          if (expectedCategory.totalDuration) {
+            expect(category.totalDuration).toEqualDuration(expectedCategory.totalDuration, 'minutes');
+          }
         });
-        expect(category.tasks).toHaveLength(expectedCategory.tasks.length);
-        expect(category.totalDuration).toEqualDuration(expectedCategory.totalDuration, 'minutes');
-      });
-      expect(log.categories).toHaveLength(testCase.assertions.categories.length);
+        expect(log.categories).toHaveLength(testCase.assertions.categories.length);
+      }
 
-      testCase.assertions.errors.forEach((expectedError, e) => {
-        expect(log.errors[e].startTime).toEqualTime(expectedError.startTime);
-        expect(log.errors[e].endTime).toEqualTime(expectedError.endTime);
-      });
-      expect(log.errors).toHaveLength(testCase.assertions.errors.length);
+      if (testCase.assertions.errors) {
+        testCase.assertions.errors.forEach((expectedError, e) => {
+          expect(log.errors[e].startTime).toEqualTime(expectedError.startTime);
+          expect(log.errors[e].endTime).toEqualTime(expectedError.endTime);
+        });
+        expect(log.errors).toHaveLength(testCase.assertions.errors.length);
+      }
 
-      testCase.assertions.gaps.forEach((expectedGap, g) => {
-        expect(log.gaps[g].startTime).toEqualTime(expectedGap.startTime);
-        expect(log.gaps[g].endTime).toEqualTime(expectedGap.endTime);
-      });
-      expect(log.gaps).toHaveLength(testCase.assertions.gaps.length);
+      if (testCase.assertions.gaps) {
+        testCase.assertions.gaps.forEach((expectedGap, g) => {
+          expect(log.gaps[g].startTime).toEqualTime(expectedGap.startTime);
+          expect(log.gaps[g].endTime).toEqualTime(expectedGap.endTime);
+        });
+        expect(log.gaps).toHaveLength(testCase.assertions.gaps.length);
+      }
 
-      testCase.assertions.overlaps.forEach((expectedOverlap, o) => {
-        expect(log.overlaps[o].startTime).toEqualTime(expectedOverlap.startTime);
-        expect(log.overlaps[o].endTime).toEqualTime(expectedOverlap.endTime);
-      });
-      expect(log.overlaps).toHaveLength(testCase.assertions.overlaps.length);
+      if (testCase.assertions.overlaps) {
+        testCase.assertions.overlaps.forEach((expectedOverlap, o) => {
+          expect(log.overlaps[o].startTime).toEqualTime(expectedOverlap.startTime);
+          expect(log.overlaps[o].endTime).toEqualTime(expectedOverlap.endTime);
+        });
+        expect(log.overlaps).toHaveLength(testCase.assertions.overlaps.length);
+      }
     });
   });
 
