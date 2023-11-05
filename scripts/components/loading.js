dd085b5548f5ada3f8430f78170a4d59b63788ed
Caleb Evans <caleb@calebevans.me>
2023-11-05 07:37:43 -0800

    Add aria-label to loading indicator

--- scripts/components/loading.js
+++ scripts/components/loading.js
@@ -2,7 +2,7 @@ import m from 'mithril';
 
 class LoadingComponent {
   view({ attrs }) {
-    return m('div.loading', attrs, [
+    return m('div.loading[aria-label="Loading..."]', attrs, [
       m('svg[viewBox="0 0 24 24"]', [
         m('title', 'Loading...'),
         m('path', { d: 'M 3,12 A 6,6 0,0,0 21,12' })
