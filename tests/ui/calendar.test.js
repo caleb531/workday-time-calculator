d88fd79dbcc28b9263f26c79c026c03b46cc7cda
Caleb Evans <caleb@calebevans.me>
2023-11-04 18:15:05 -0700

    Add basic test for calendar panel

---
+++ tests/ui/calendar.test.js
@@ -0,0 +1,41 @@
+import moment from 'moment';
+import m from 'mithril';
+import {
+  getByText,
+  getAllByText,
+  getByRole,
+  waitFor,
+  fireEvent
+} from '@testing-library/dom';
+import {
+  forEachTestCase,
+  renderApp,
+  unmountApp,
+  applyLogContentsToApp,
+  formatDuration,
+  formatTime
+} from '../utils.js';
+
+describe('log calendar', () => {
+  afterEach(async () => {
+    await unmountApp();
+    localStorage.clear();
+  });
+
+  it('should open', async () => {
+    await renderApp();
+    await waitFor(() => {
+      expect(
+        getByRole(document.body, 'button', { name: 'Toggle Calendar' })
+      ).toBeInTheDocument();
+    });
+    fireEvent.click(
+      getByRole(document.body, 'button', { name: 'Toggle Calendar' })
+    );
+    await waitFor(() => {
+      expect(
+        getByText(document.body, moment().format('MMMM YYYY'))
+      ).toBeInTheDocument();
+    });
+  });
+});
