dcc19ddcfa3f7a04174dd788b3c7f60dfdcf64ed
Caleb Evans <caleb@calebevans.me>
2023-11-10 15:24:57 -0800

    Add test for changing color theme

--- tests/ui/preferences.test.js
+++ tests/ui/preferences.test.js
@@ -1,6 +1,22 @@
-import { findByRole, findByText } from '@testing-library/dom';
+import {
+  findByLabelText,
+  findByRole,
+  findByText,
+  waitFor
+} from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
+import * as idbKeyval from 'idb-keyval';
 import { renderApp, unmountApp } from '../utils.js';
+
+async function openPreferences() {
+  const getToolsControl = () =>
+    findByRole(document.body, 'button', { name: 'Toggle Tools Menu' });
+  const getPrefsMenuItem = () => findByText(document.body, 'Preferences');
+  expect(await getToolsControl()).toBeInTheDocument();
+  userEvent.click(await getToolsControl());
+  expect(await getPrefsMenuItem()).toBeInTheDocument();
+  userEvent.click(await getPrefsMenuItem());
+}
 
 describe('app preferences', () => {
   afterEach(async () => {
@@ -9,15 +25,23 @@ describe('app preferences', () => {
 
   it('should open', async () => {
     await renderApp();
-    const getToolsControl = () =>
-      findByRole(document.body, 'button', { name: 'Toggle Tools Menu' });
-    const getPrefsMenuItem = () => findByText(document.body, 'Preferences');
-    expect(await getToolsControl()).toBeInTheDocument();
-    userEvent.click(await getToolsControl());
-    expect(await getPrefsMenuItem()).toBeInTheDocument();
-    userEvent.click(await getPrefsMenuItem());
+    await openPreferences();
     expect(
       await findByRole(document.body, 'heading', { name: 'Preferences' })
     ).toBeInTheDocument();
+  });
+
+  it('should change theme', async () => {
+    await renderApp();
+    expect(document.body).toHaveClass('color-theme-blue');
+    await openPreferences();
+    await userEvent.click(await findByLabelText(document.body, 'Rose'));
+    await waitFor(() => {
+      expect(document.body).toHaveClass('color-theme-rose');
+    });
+    expect(await idbKeyval.get('wtc-prefs')).toHaveProperty(
+      'colorTheme',
+      'rose'
+    );
   });
 });
