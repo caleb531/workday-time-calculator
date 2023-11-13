546f5869530d6a2d3b03a9a90e1ad00ff8eee4e8
Caleb Evans <caleb@calebevans.me>
2023-11-13 11:40:37 -0800

    Parameterize reminder tests

--- tests/ui/reminders.test.js
+++ tests/ui/reminders.test.js
@@ -7,47 +7,51 @@ import {
   unmountApp
 } from '../utils.js';
 
+// Conversion factors
 const S_IN_M = 60;
 const MS_IN_S = 1000;
 
-describe('reminder system', () => {
+describe.each([
+  { label: 'Every 15 minutes', minutes: 15 },
+  { label: 'Every half-hour', minutes: 30 },
+  { label: 'Every hour', minutes: 60 }
+])('reminder system with $minutes-minute reminders', ({ label, minutes }) => {
   beforeEach(async () => {
     vi.useFakeTimers({ shouldAdvanceTime: true });
   });
+
   afterEach(async () => {
     vi.useRealTimers();
     Notification._resetPermissions();
     await unmountApp();
   });
 
-  it('should enable reminders', async () => {
+  it('should enable', async () => {
     await renderApp();
     await openPreferences();
     Notification._grantWhenRequested();
-    await clickPreferenceOption('Reminder Interval', 'Every 15 minutes');
+    await clickPreferenceOption('Reminder Interval', label);
     expect(Notification).toHaveBeenCalledWith('Workday Time Calculator', {
-      body: 'Reminder set to show every 15 minutes!',
+      body: `Reminder set to show every ${minutes} minutes!`,
       icon: 'app-icon.png'
     });
     expect(Notification).toHaveBeenCalledTimes(1);
   });
+
   it('should request permissions if preference is enabled but permissions have not yet been requested', async () => {
     vi.spyOn(Notification, 'requestPermission');
-    await setPreferences({ reminderInterval: 15 });
+    await setPreferences({ reminderInterval: minutes });
     await renderApp();
     await waitFor(() => {
       expect(Notification.requestPermission).toHaveBeenCalled();
     });
   });
-  it('should spawn 15-minute reminder when 15 minutes elapses', async () => {
-    const minutes = 15;
+
+  it('should spawn reminder when %i minutes elapses', async () => {
     await renderApp();
     await openPreferences();
     Notification._grantWhenRequested();
-    await clickPreferenceOption(
-      'Reminder Interval',
-      `Every ${minutes} minutes`
-    );
+    await clickPreferenceOption('Reminder Interval', label);
     vi.advanceTimersByTime(minutes * S_IN_M * MS_IN_S);
     expect(Notification).toHaveBeenCalledWith('Workday Time Calculator', {
       body: 'Remember to update your log!',
@@ -55,15 +59,12 @@ describe('reminder system', () => {
     });
     expect(Notification).toHaveBeenCalledTimes(2);
   });
-  it('should re-spawn reminder at the designated interval', async () => {
-    const minutes = 15;
+
+  it('should re-spawn reminder every %i minutes', async () => {
     await renderApp();
     await openPreferences();
     Notification._grantWhenRequested();
-    await clickPreferenceOption(
-      'Reminder Interval',
-      `Every ${minutes} minutes`
-    );
+    await clickPreferenceOption('Reminder Interval', label);
     vi.advanceTimersByTime(2 * minutes * S_IN_M * MS_IN_S);
     expect(Notification).toHaveBeenCalledWith('Workday Time Calculator', {
       body: 'Remember to update your log!',
