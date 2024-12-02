3a446e1a96cb6a1fba695acda053c6f29cdd0989
Caleb Evans <caleb@calebevans.me>
2024-12-02 15:04:24 -0800

    Fix styling of loading indicators

--- scripts/components/loading.jsx
+++ scripts/components/loading.jsx
@@ -1,9 +1,13 @@
-import m from 'mithril';
+import clsx from 'clsx';
 
 class LoadingComponent {
   view({ attrs }) {
     return (
-      <div className="loading" aria-label="Loading..." {...attrs}>
+      <div
+        aria-label="Loading..."
+        {...attrs}
+        className={clsx(['loading', attrs.className])}
+      >
         <svg viewBox="0 0 24 24">
           <title>Loading...</title>
           <path d="M 3,12 A 6,6 0,0,0 21,12" />
